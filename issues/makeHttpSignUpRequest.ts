15c0cd082eb0c797c6e8db360c03ef6f23fe68a3
Juha Suni <juha.suni@showell.com>
2022-01-08 17:37:50 +0200

    Rename issue function

--- issues/makeHttpSignUpRequest.ts
+++ issues/makeHttpSignUpRequest.ts
@@ -4,14 +4,23 @@
  * Signing up with account "John&Son" seems to fail for some reason.
  * Also, someone messed the authentication in the recent commit and
  * it doesn't seem to work with our OAuth 2 based API.
+ *
+ * Note, that our API supports both JSON and Form data.
  */
 
-export function getHttpSignUpRequest(
+export function makeHttpSignUpRequest(
   bearerToken: string,
   accountName: string,
-  email: string
+  email: string,
+  contentType: "form" | "json"
 ): string {
-  const body = `name=${accountName}&userEmail=${email}`;
+  let body = "";
+
+  if (contentType === "form") {
+    body = `name=${accountName}&userEmail=${email}`;
+  } else {
+    body = JSON.stringify({ name: accountName, userEmail: email });
+  }
 
   return `POST /1/sign-up HTTP/1.1
     Host: api.myapp
