34cd9e4bd7483a104ee6ec3fe7b645d57c15ee5d
Jonjo7Dia <jonosphgames@gmail.com>
2022-08-25 20:55:11 -0300

    Outlining issues and writing out initial thoughts

--- issues/makeHttpSignUpRequest.ts
+++ issues/makeHttpSignUpRequest.ts
@@ -1,3 +1,14 @@
+//Issues:
+// 1. Signing up with account "John&Son" seems to fail for some reason when using makeHttpSignUpRequest.
+// 2. Also, someone messed the authentication in the recent commit and it doesn't seem to work with our OAuth 2 based API.
+
+//Initial Thoughts with Issue #1,
+//looking at the name John&Son I'm assuming that the "&" charachter is messing up with the contents's body when sending a request
+//upon quick research that guess is confirmed. and to fix this issue I propose two solutions 
+//1. when creating the account ensure the names do not include any special characters that could messing with request body
+// I do not like this solution because as its not really solving the bug. ++ we are seeing now with likes of elon musks baby that names might have special characters.
+//2. Therefore we should create a function to ensure that the account name can be "translated" so the "computer" can understand that the accountname has special characters and can handle its
+
 export function makeHttpSignUpRequest(
   bearerToken: string,
   accountName: string,
